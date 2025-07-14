38940c43a3d1bfb93094195126d813e7e3897a77
vp67082 <vp67082@gmail.com>
2025-07-14 09:15:58 +0200

    Fix React Router basename for base path

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/withmaind/">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
