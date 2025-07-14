be58926609041e05661178f47b75406345416d26
vp67082 <vp67082@gmail.com>
2025-07-14 09:14:17 +0200

    Fix syntax error in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-base: "/withmaind/"
+base: "/withmaind/",
   server: {
     host: "::",
     port: 8080,
